44acfc395d377993d8a375e15a6cec4a29469829
fattah <m.agahi@outlook.com>
2020-09-18 22:39:32 +0430

    kjhgf

--- gw/gw.sh
+++ gw/gw.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-echo test
-sleep 300
+temp=`echo /gw-input/$SRCFILE`
+echo "$temp - this is the recunstructed file" > /gw-output/files/$SRCFILE
+report="$SRCFILE.xml"
+echo "and this is the report of $SRCFILE" > /gw-output/reports/$report
